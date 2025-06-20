57b7deeb0d3d309384e9b7ba0ccb7c7a69e9173b
Brayan <btellez948@gmail.com>
2025-06-19 18:16:27 -0600

    [Feature] Adding translation to all pages.

--- src/interface/AppLangs.ts
+++ src/interface/AppLangs.ts
@@ -8,8 +8,52 @@ export interface IAboutMeTranslation {
     videos: string
     hobbies: string
     projects: string
+    web: string
+}
+
+export interface IProjectsTranslation {
+    title: string
+    date: string
+    description: string
+    details: string
+}
+
+export interface IBlogsTranslation {
+    title: string
+    time: string
+}
+
+export interface ILearnTranslation {
+    title: string
+    repository: string
+    playlist: string
+}
+
+export interface IProjectDetailsTranslation {
+    section: string
+    date: string
+    about: string
+    galery: string
+    objectives: string
+    links: string
+    technlologies: string
+    comments: string
+    clients: string
+    publish: string
+}
+
+export interface IBlogPostTranslation {
+    section: string
+    publish: string
+    duration: string
+    time: string
 }
 
 export interface IAppTranslation {
     about: IAboutMeTranslation
+    projects: IProjectsTranslation
+    blogs: IBlogsTranslation
+    learn: ILearnTranslation
+    project_details: IProjectDetailsTranslation
+    blog_post: IBlogPostTranslation
 }
