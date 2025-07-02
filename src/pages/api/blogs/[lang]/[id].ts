df81ab0b0792c5ec19c4a02777aa4adf536d50d5
Brayan <btellez948@gmail.com>
2025-07-01 23:58:36 -0600

    [Hot Fix] Prevent no blogs convertion.

--- src/pages/api/blogs/[lang]/[id].ts
+++ src/pages/api/blogs/[lang]/[id].ts
@@ -1,5 +1,5 @@
 import type { APIContext, APIRoute } from 'astro';
-import type { IBlog } from '../../../../interface/Blog';
+import type { IBlog, IBlogDefinition } from '../../../../interface/Blog';
 
 import STATUS from '../../../../status/statuscode';
 
@@ -7,7 +7,7 @@ import Blogs from "../../../../../public/assets/blogs/blogs.json";
 import LANGS_SUPPORTED from '../../../../utils/langs';
 
 export async function getStaticPaths() {
-  return Blogs.flatMap( id => LANGS_SUPPORTED.map( lang => ({ params: { lang, id: id.id } })) );
+  return (Blogs as IBlogDefinition[]).flatMap( id => LANGS_SUPPORTED.map( lang => ({ params: { lang, id: id.id } })) );
 }
 
 export const GET: APIRoute = async ({ params, url }: APIContext) => {
--- src/router/blogs.ts
+++ src/router/blogs.ts
@@ -2,8 +2,10 @@ import Blogs from "../../public/assets/blogs/blogs.json";
 import LANGS_SUPPORTED from "../utils/langs";
 import baseUrl from "./_base";
 
+import type { IBlogDefinition } from "../interface/Blog";
+
 export const GetBlogsPaths = () => {
-    return Blogs.flatMap( id => 
+    return (Blogs as IBlogDefinition[]).flatMap( id => 
         LANGS_SUPPORTED.map( lang => 
             `${baseUrl}/${lang}/blogs/${id.id}`
         )
